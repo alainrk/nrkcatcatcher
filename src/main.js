676f0b30e5e203d373ee11e9f16b225343b5d7bd
Alain <alain.devcs@gmail.com>
2017-06-07 15:18:53 +0200

    structure and lib

---
+++ src/main.js
@@ -0,0 +1,14 @@
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, null, { preload: preload, create: create, update: update });
+var cat, catcher, cursors, txtScore, score;
+
+function preload() {
+
+}
+
+function create() {
+
+}
+
+function update() {
+
+}
